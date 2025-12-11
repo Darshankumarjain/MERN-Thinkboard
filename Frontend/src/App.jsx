6ae47f013dca1e919c197c815de4805b50793c0a
DARSHANKUMAR JAIN <145105070+Darshankumarjain@users.noreply.github.com>
2025-12-11 19:51:34 +0530

    App.jsx update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import Homepage from './pages/Homepage';
+import HomePage from './pages/HomePage.jsx';
 import CreatePage from './pages/CreatePage';
 import NoteDetailpage from './pages/NoteDetailpage';
 import { Route, Routes } from 'react-router';
@@ -12,7 +12,7 @@ const App = () => {
     <div className='relative h-full w-full'>
        <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/note/:id" element={<NoteDetailpage />} />
       </Routes>
